22d2eee11b8e2076022f32668f31cf79340280bb
dako-t <57984478+dako-t@users.noreply.github.com>
2023-10-22 17:37:49 +0800

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,4 +177,10 @@ $(function () {
   $(".back__btn").click(function () {
     history.back();
   });
+
+  document.body.addEventListener('mousemove', function(e){
+		var mX = e.pageX + 'px';
+		var mY = e.pageY + 'px';
+		document.getElementsByClassName('cursor')[0].style.transform = 'translate3d(' + mX + ',' + mY  + ',' +  '0px)';
+	});
 });
